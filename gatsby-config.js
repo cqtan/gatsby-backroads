f9d99fd96f4e6139295c94eb9d29d24b4ae72258
ctan <s0547139@htw-berlin.de>
2019-10-14 00:55:40 +0200

    Add gatsby-image example for fixed and fluid images in Blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout.component"
+import Images from "../examples/images"
 
 const Blog = props => {
   return (
     <Layout>
       <div>Blog</div>
+      <Images />
     </Layout>
   )
 }
