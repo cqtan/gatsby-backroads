140d94f552aea05015827dd6c2526e3e08df8aa6
ctan <s0547139@htw-berlin.de>
2019-10-16 00:49:34 +0200

    Update meta data

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -29,11 +29,7 @@ const SEO = ({ title, description }) => {
   } = site.siteMetadata
 
   return (
-    <Helmet
-      htmlAttributes={{ lang: "en" }}
-      title={`${title} | ${siteTitle}
-  `}
-    >
+    <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
       {/* facebook cards */}
@@ -46,7 +42,7 @@ const SEO = ({ title, description }) => {
       <meta property="og:image:height" content="300" />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={twitterUserName} />
+      <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
